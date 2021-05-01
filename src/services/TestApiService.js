9cc08fb97619f921f4905bba916d307cd37edcb4
Ronan MOREL <ronanmorel@outlook.fr>
2021-05-01 15:00:55 +0200

    #1 Readd keycloak configuration

--- src/services/TestApiService.js
+++ src/services/TestApiService.js
@@ -3,11 +3,11 @@ import ApiService from './ApiService';
 export default class UserService extends ApiService {
 
   public() {
-    return this.httpGet('/api/public')
+    return this.httpGet('/api/v1/public')
   }
 
   private() {
-    return this.httpGet('/api/test')
+    return this.httpGet('/api/v1/test')
   }
 
 }
